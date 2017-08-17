a04d96901426754d54266322f86833340ede5acc
Kievbuy <kievbuy@gmail.com>
2017-08-17 19:39:26 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 require 'sqlite3'
+
+set :database, 'sqlite3:pizza.db'
+
+class Product < ActiveRecord::Base
+end
+
 
 
 get '/' do
